a167df7cb0d6f60fb3cc2e32adf9e989b0da1ffd
Deepak Rai <103811779+StrawHATDEEPAK@users.noreply.github.com>
2023-06-19 16:17:52 +0530

    Added more fields

---
+++ lib/auth/constants.dart
@@ -0,0 +1,2 @@
+const List<String> batchList = ['BCS', 'IMT', 'IMG'];
+const List<String> yearJoinedList = ['2019', '2020', '2021', '2022', '2023'];
--- lib/auth/screens/register_screen.dart
+++ lib/auth/screens/register_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
+import 'package:summer_project/auth/constants.dart';
+import 'package:summer_project/auth/widgets/image_pick_preview.dart';
 import 'package:summer_project/common_widgets/toast.dart';
+import 'dart:developer' as dev;
 
 import '../widgets/auth_textfield_widget.dart';
 
@@ -13,41 +17,104 @@ class RegisterScreen extends StatefulWidget {
 
 class _RegisterScreenState extends State<RegisterScreen> {
   final formKey = GlobalKey<FormState>();
-
   bool isTeacher = false;
+
   final _emailTextController = TextEditingController();
-  final _passwordTextController = TextEditingController();
-  final _confirmPasswordTextController = TextEditingController();
+  final _studentNameTextController = TextEditingController();
+  final _classRollNoTextController = TextEditingController();
+  String yearJoined = '2021';
+  String batch = 'BCS';
 
   @override
   void dispose() {
     super.dispose();
     _emailTextController.dispose();
-    _passwordTextController.dispose();
+    _studentNameTextController.dispose();
+    _classRollNoTextController.dispose();
   }
+
+  // void pickImageFromGallery(
+  //     bool forStudentImage, bool forStudentImageId) async {
+  //   final XFile? image =
+  //       await _imagePicker.pickImage(source: ImageSource.camera);
+
+  //   if (image != null && forStudentImage) {
+  //     studentImage = image;
+  //   } else if (image != null && forStudentImageId) {
+  //     studentIdImage = image;
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
     // AuthProvider auth = Provider.of<AuthProvider>(context);
 
     final emailField = AuthTextFieldWidget(
+        isEmail: true,
         prefixIcon: const Icon(Icons.email),
         textEditingController: _emailTextController,
         hintText: 'Enter Email',
         textInputType: TextInputType.emailAddress);
 
-    final passwordField = AuthTextFieldWidget(
-        prefixIcon: const Icon(Icons.lock_rounded),
-        textEditingController: _passwordTextController,
-        hintText: 'Enter Password',
-        textInputType: TextInputType.visiblePassword);
+    final name = AuthTextFieldWidget(
+        isName: true,
+        prefixIcon: const Icon(Icons.person),
+        textEditingController: _studentNameTextController,
+        hintText: "Enter Student's Name",
+        textInputType: TextInputType.name);
 
-    final confirmPassword = AuthTextFieldWidget(
-        prefixIcon: const Icon(Icons.lock_rounded),
-        textEditingController: _confirmPasswordTextController,
-        hintText: 'Confirm Password',
-        textInputType: TextInputType.visiblePassword);
+    final classRollNoField = Expanded(
+      child: AuthTextFieldWidget(
+          textEditingController: _classRollNoTextController,
+          hintText: 'Enter Class Roll No.',
+          textInputType: TextInputType.number),
+    );
 
+    DropdownButton<String> yearJoinedDropDown() {
+      List<DropdownMenuItem<String>> dropdownItems = [];
+      for (String year in yearJoinedList) {
+        var newItem = DropdownMenuItem(
+          value: year,
+          child: Text(year),
+        );
+        dropdownItems.add(newItem);
+      }
+
+      return DropdownButton<String>(
+        value: yearJoined,
+        items: dropdownItems,
+        onChanged: (value) {
+          setState(() {
+            yearJoined = value.toString();
+          });
+        },
+      );
+    }
+
+    DropdownButton<String> batchDropDown() {
+      List<DropdownMenuItem<String>> dropdownItems = [];
+      for (String batch in batchList) {
+        var newItem = DropdownMenuItem(
+          value: batch,
+          child: Text(batch),
+        );
+        dropdownItems.add(newItem);
+      }
+
+      return DropdownButton<String>(
+        value: batch,
+        items: dropdownItems,
+        onChanged: (value) {
+          setState(() {
+            batch = value.toString();
+          });
+        },
+      );
+    }
+
+    final rollNoWidget = Row(
+      children: [yearJoinedDropDown(), batchDropDown(), classRollNoField],
+    );
     var loading = Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: const <Widget>[
@@ -55,32 +122,12 @@ class _RegisterScreenState extends State<RegisterScreen> {
       ],
     );
 
-    final forgotLabel = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        TextButton(
-          child: const Text("Forgot password?",
-              style: TextStyle(fontWeight: FontWeight.w300)),
-          onPressed: () {},
-        ),
-        TextButton(
-          child: const Text("Sign in",
-              style: TextStyle(fontWeight: FontWeight.w300)),
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/login');
-          },
-        ),
-      ],
-    );
-
     void doRegister() {
       final form = formKey.currentState;
       if (form != null) {
         if (form.validate()) {
-          if (_confirmPasswordTextController.text !=
-              _passwordTextController.text) {
-            showToast('Password Do not Match');
-            _confirmPasswordTextController.clear();
+          if (true) {
+// if textfield input is error filled
           } else {
             form.save();
             // auth.register(_username, _password, _confirmPassword).then((response) {
@@ -110,34 +157,54 @@ class _RegisterScreenState extends State<RegisterScreen> {
             padding: const EdgeInsets.all(40.0),
             child: Form(
               key: formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 215.0),
-                  const Text("Email"),
-                  const SizedBox(height: 5.0),
-                  emailField,
-                  const SizedBox(height: 15.0),
-                  const Text("Password"),
-                  const SizedBox(height: 10.0),
-                  passwordField,
-                  const SizedBox(height: 15.0),
-                  const Text("Confirm Password"),
-                  const SizedBox(height: 10.0),
-                  confirmPassword,
-                  const SizedBox(height: 20.0),
-                  //  auth.registeredInStatus == Status.Registering
-                  //       ? loading
-                  //       :
-                  ElevatedButton(
-                    onPressed: () {
-                      doRegister();
-                    },
-                    child: const Text('Register'),
-                  ),
-                  const SizedBox(height: 5.0),
-                  forgotLabel
-                ],
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 215.0),
+                    const Text("Email"),
+                    const SizedBox(height: 5.0),
+                    emailField,
+                    const SizedBox(height: 15.0),
+                    const Text("Student's Name"),
+                    const SizedBox(height: 10.0),
+                    name,
+                    const SizedBox(height: 20.0),
+                    const Text("Roll No."),
+                    const SizedBox(height: 5.0),
+                    rollNoWidget,
+                    const SizedBox(height: 15.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text("Student's PhotoGraph"),
+                        ImagePicKPreviewWidget(
+                          context: context,
+                          forStudentImage: true,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 15.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text("Student's ID PhotoGraph"),
+                        ImagePicKPreviewWidget(
+                          context: context,
+                          forStudentIdImage: true,
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 5.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        doRegister();
+                      },
+                      child: const Text('Register'),
+                    ),
+                    const SizedBox(height: 5.0),
+                  ],
+                ),
               ),
             ),
           ),
