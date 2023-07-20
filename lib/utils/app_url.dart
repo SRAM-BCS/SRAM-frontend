3d3ba23ae00110170e8f1b8c3aa5cc7aa7c05dde
Deepak Rai <103811779+StrawHATDEEPAK@users.noreply.github.com>
2023-07-20 21:39:06 +0530

    Updated baseUrl String with env var

--- lib/utils/app_url.dart
+++ lib/utils/app_url.dart
@@ -1,29 +1,29 @@
+import 'dotenv.dart';
+
 class AppUrl {
-  static const String baseURL =
-      'https://sram-backend-4vcn5hxtea-uc.a.run.app/api/';
+  static final String baseURL = DotenvVar.baseUrl;
   //student urls
   static const String studentUrl = 'student';
-  static const String studentLogin = "$baseURL$studentUrl/login";
-  static const String studentRegister = "$baseURL$studentUrl/register/";
-  static const String getStudentwithEmail = "$baseURL$studentUrl/get/";
-  static const String getStudentCourse = "$baseURL$studentUrl/course";
+  static const String studentLogin = "$studentUrl/login";
+  static const String studentRegister = "$studentUrl/register/";
+  static const String getStudentwithEmail = "$studentUrl/get/";
+  static const String getStudentCourse = "$studentUrl/course";
   //student attendance urls
-  static const String markAttendance = "$baseURL$studentUrl/attendance";
-  static const String getAttendance = "$baseURL$studentUrl/attendance/";
-  static const String faceCamera = "$baseURL$studentUrl/face/verification/";
+  static const String markAttendance = "$studentUrl/attendance";
+  static const String getAttendance = "$studentUrl/attendance/";
+  static const String faceCamera = "$studentUrl/face/verification/";
 
   //faculty urls
   static const String facultyUrl = 'faculty';
-  static const String getFaculty = "$baseURL$facultyUrl/get";
-  static const String facultyCodeStatus =
-      "$baseURL$facultyUrl/faculty/code/status/";
-  static const String facultyLogin = "$baseURL$facultyUrl/login";
-  static const String facultyBatchCourse = "$baseURL$facultyUrl/batch/course";
+  static const String getFaculty = "$facultyUrl/get";
+  static const String facultyCodeStatus = "$facultyUrl/faculty/code/status/";
+  static const String facultyLogin = "$facultyUrl/login";
+  static const String facultyBatchCourse = "$facultyUrl/batch/course";
   static const String facultyBatchCourseAttendance =
-      "$baseURL$facultyUrl/batch/course/attendance";
+      "$facultyUrl/batch/course/attendance";
 
   //common urls
-  static const String otpGenerate = "$baseURL$studentUrl/otp/generate/";
-  static const String otpVerify = "$baseURL$studentUrl/otp/verify/";
-  static const String forgotPassword = "$baseURL$studentUrl/forgot_password/";
+  static const String otpGenerate = "$studentUrl/otp/generate/";
+  static const String otpVerify = "$studentUrl/otp/verify/";
+  static const String forgotPassword = "$studentUrl/forgot_password/";
 }
